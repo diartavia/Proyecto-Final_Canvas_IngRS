ded7e8a418c52c671cd5a0ba9c5835749c5f9b1d
diartavia <diartavia@gmail.com>
2025-06-28 18:56:36 -0600

    Solo se agrego una carpeta nueva

--- src/Pantallas/LogIn/LoginProfesor.java
+++ src/Pantallas/LogIn/LoginProfesor.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package Pantallas;
+package Pantallas.LogIn;
 
 import javax.swing.JOptionPane;
 
